c13c4dd656b0a75b3c23c127b40dd7d0484a0892
mortenm12 <mortenm12@hotmail.com>
2015-05-04 18:54:38 +0200

    User startet

---
+++ StregSystem/StregSystem/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StregSystem
+{
+    class User : IComparable
+    {
+        /// <summary>
+        /// The ID number of the User.
+        /// </summary>
+        public readonly long Id;
+
+        /// <summary>
+        /// The FirstName of the User.
+        /// </summary>
+        public string FirstName { get; private set; }
+
+        /// <summary>
+        /// The LastName of the User.
+        /// </summary>
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// The UserName of the User.
+        /// </summary>
+        public string UserName { get; private set; }
+
+        /// <summary>
+        /// The Email adress og the User.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The money balance of the User.
+        /// </summary>
+        public int Balance { get; set; }
+
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Id == (User)obj.Id;
+        }
+    }
+}
